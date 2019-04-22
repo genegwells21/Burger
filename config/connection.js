ec0f25949449bda40448b405bce1b6739b8fceb6
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-04-21 23:52:10 -0400

    initial file upload

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+var mysql = require("mysql");
+    if(process.env.JAWSDB_URL)  {
+        connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+    } else  {
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+}
+connection.connect();
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured) VALUES ("hamburger", false), ("veggieburger", false);
